f1a9daadc7ac3461dda301dcac770e7025aebdfb
fuhao1517 <53571596+fuhao1517@users.noreply.github.com>
2019-09-21 21:12:47 +0800

    第一次提交

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="colorRed">
+    <div class="box"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style>
+.box {
+  width: 10vw;
+  height: 10vw;
+  background: gray;
+}
+</style>
