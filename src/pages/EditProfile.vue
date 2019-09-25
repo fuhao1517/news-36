2b844ea1b38b0d6edc6a473fd8d717ab2cf93d4d
fuhao1517 <53571596+fuhao1517@users.noreply.github.com>
2019-09-25 17:07:48 +0800

    22、渲染编辑资料页的数据

--- src/pages/EditProfile.vue
+++ src/pages/EditProfile.vue
@@ -4,12 +4,12 @@
     <HeaderNormal title="编辑资料" />
     <!-- 头像 -->
     <div class="head">
-      <img src="../../static/dog.jpg" alt />
+      <img :src="profile.head_img" alt />
     </div>
     <!-- 调用条形组件 -->
-    <CellBar label="昵称" text="火星网友" />
-    <CellBar label="密码" text="123" />
-    <CellBar label="性别" text="男" />
+    <CellBar label="昵称" :text="profile.nickname" />
+    <CellBar label="密码" :text="profile.password" type="password" />
+    <CellBar label="性别" :text="profile.gender===1?'男':'女'" />
   </div>
 </template>
 
@@ -19,9 +19,37 @@ import HeaderNormal from "@/components/HeaderNormal";
 import CellBar from "@/components/CellBar";
 
 export default {
+  data() {
+    return {
+      /* 用户详情 */
+      profile: {}
+    };
+  },
   components: {
     HeaderNormal,
     CellBar
+  },
+  mounted() {
+    /* 请求个人资料接口 */
+    this.$axios({
+      url: "/user/" + localStorage.getItem("user_id"),
+      /* 添加头信息 */
+      headers: {
+        Authorization: localStorage.getItem("token")
+      }
+    }).then(res => {
+      const { data } = res.data;
+      if (data) {
+        /* 保存data */
+        this.profile = data;
+        /* 如果用户有头像 */
+        if (data.head_img) {
+          this.profile.head_img = this.$axios.defaults.baseURL + data.head_img;
+        } else {
+          this.profile.head_img = "./static/dog.jpg";
+        }
+      }
+    });
   }
 };
 </script>
