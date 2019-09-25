f6b7bb8b21e5f2059740d751a48b23acbe0bb010
fuhao1517 <53571596+fuhao1517@users.noreply.github.com>
2019-09-25 23:11:21 +0800

    35、用户评论回复的布局

--- src/pages/UserComment.vue
+++ src/pages/UserComment.vue
@@ -1,33 +1,25 @@
 <template>
   <div>
-      <HeaderNormal title="我的跟帖"/>
+    <HeaderNormal title="我的跟帖" />
 
-      <!-- 评论的列表 -->
-      <div class="comment-item">
-          <div class="time">2019-9-25 10:10</div>
+    <!-- 评论的列表 -->
+    <div class="comment-item">
+      <div class="time">2019-9-25 10:10</div>
 
-          <!-- 当前的评论回复的评论 -->
-          <div class="parent">
-              <div class="parent-title">
-                  @: 彩票研究员
-              </div>
-              <div class="parent-content">
-                  今天买什么码？
-              </div>
-          </div>
-
-          <!-- 个人评论的内容 -->
-          <div class="content">
-              不知道
-          </div>
-
-          <div class="article-link">
-            <router-link to="#">
-                原文：今天买什么码？今天买什么码？今天买什么码？今天买什么码？今天买什么码？
-            </router-link>
-            <span class="iconfont iconjiantou1"></span>
-          </div>
+      <!-- 当前的评论回复的评论 -->
+      <div class="parent">
+        <div class="parent-title">@: 彩票研究员</div>
+        <div class="parent-content">今天买什么码？</div>
       </div>
+
+      <!-- 个人评论的内容 -->
+      <div class="content">不知道</div>
+
+      <router-link to="#" class="article-link">
+        <p>原文：今天买什么码？今天买什么码？今天买什么码？今天买什么码？今天买什么码？</p>
+        <span class="iconfont iconjiantou1"></span>
+      </router-link>
+    </div>
   </div>
 </template>
 
@@ -35,11 +27,46 @@
 // 导入头部
 import HeaderNormal from "@/components/HeaderNormal";
 export default {
-    components: {
-        HeaderNormal
-    }
-}
+  components: {
+    HeaderNormal
+  }
+};
 </script>
 
-<style>
+<style scoped lang="less">
+.comment-item {
+  padding: 10px;
+  font-size: 14px;
+  line-height: 1.8;
+  border-bottom: 1px #ccc solid;
+  .time {
+    font-size: 12px;
+    color: #999;
+    margin-bottom: 5px;
+  }
+  .parent {
+    background: #eee;
+    padding: 10px;
+    margin-bottom: 5px;
+    .parent-title {
+      font-size: 12px;
+      color: #999;
+    }
+  }
+  .content {
+    margin-bottom: 5px;
+  }
+  .article-link {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    font-size: 12px;
+    color: #999;
+    p {
+      overflow: hidden;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+    }
+  }
+}
 </style> 
