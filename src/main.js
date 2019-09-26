028c0ed6b69d93b8c5fa0682159a99214620a364
fuhao1517 <53571596+fuhao1517@users.noreply.github.com>
2019-09-26 14:24:06 +0800

    37、首页头部布局

--- src/main.js
+++ src/main.js
@@ -10,13 +10,17 @@ import {
 import axios from "axios"
 
 // 导入组件
+/* 后台的页面 */
 import App from "@/App";
 import Login from "@/pages/Login";
 import Register from "@/pages/Register";
 import Personal from "@/pages/Personal";
 import EditProfile from "@/pages/EditProfile";
 import UserFollow from "@/pages/UserFollow";
-import UserComment  from "@/pages/UserComment";
+import UserComment from "@/pages/UserComment";
+
+/* 前台页面 */
+import Index from "@/pages/Index";
 
 
 
@@ -52,7 +56,11 @@ const routes = [{
     },
     {
         path: "/user_comment",
-        component: UserComment 
+        component: UserComment
+    },
+    {
+        path: "/",
+        component: Index
     },
 
 
